42ecb1d1284d7fd5fb855b4904fb5506c55d8fbd
John Hutchinson <john.d.hutchinson@outlook.com>
2018-01-13 18:55:59 +0000

    Changed email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'john.d.hutchinson@outlook.com'
+    default to: 'jhutchdhutchinson@gmail.com'
    
     def contact_email(name, email, body) 
        @name = name
